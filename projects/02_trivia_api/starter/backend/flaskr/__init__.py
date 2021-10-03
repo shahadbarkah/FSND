e12eba523ec4ce2c477df0b790decfdbd67046da
shahahd <47734916+shahahd@users.noreply.github.com>
2021-10-03 19:54:13 +0300

    add error handler

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -255,6 +255,13 @@ def create_app(test_config=None):
       "message": "bad request"
       }), 400
     
+  @app.errorhandler(500)
+  def bad_request(error):
+    return jsonify({
+     "success": False,
+      "error": 500,
+      "message": "internal server error"
+      }), 400  
   return app
 
     
